e300fc9a58a8fe5911b4357bd4e5b87c63f9f344
OliverKragh <98881951+OliverKragh@users.noreply.github.com>
2023-01-31 14:20:29 +0100

    d

---
+++ Assets/Scenes/ass.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rull gay
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
