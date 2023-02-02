5565e5e72bbced29b19cf76e5aecd5f8bb77c925
OliverKragh <98881951+OliverKragh@users.noreply.github.com>
2023-02-02 09:23:58 +0100

    d

--- Unity projekt/Assets/Scenes/scripts/PlayerController.cs
+++ Unity projekt/Assets/Scenes/scripts/PlayerController.cs
@@ -2,39 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//movement
+public class PlayerController : MonoBehaviour
+{ 
 
+//movement
 public float horizontalInput;
 public float verticalInput;
-private float speed = 20;
+private float speed = 20000;
 
-public class PlayerController : MonoBehaviour
-{
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
+
+
     }
 
     private void Movement()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-       
+
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.up * Time.deltaTime * speed * horizontalInput);
 
-
-
-
     }
-
-
 }
+
+
