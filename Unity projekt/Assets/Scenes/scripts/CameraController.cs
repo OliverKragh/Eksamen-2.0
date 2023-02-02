c2abdbe9b695b42c559a3526877f2e2efcb8fb7f
JeppeMW <jeppewortmann@outlook.dk>
2023-02-02 09:25:29 +0100

    Kam / move
    Kameraet virker
    Dude kan flytte sig og rotere og det er bare fed

---
+++ Unity projekt/Assets/Scenes/scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //MOUSE SENS
+    
+    public float mouseVerticalSpeed = 2.0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MUS
+        
+        float v = mouseVerticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(v, 0, 0);
+    }
+}
--- Unity projekt/Assets/Scenes/scripts/PlayerController.cs
+++ Unity projekt/Assets/Scenes/scripts/PlayerController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{ 
+{
+    //KEYBOARD MOVEMENT
+    public float horizontalInput;
+    public float verticalInput;
+    private float speed = 20;
 
-//movement
-public float horizontalInput;
-public float verticalInput;
-private float speed = 20000;
 
-
+    //
+    public float mouseHorizontalSpeed = 2.0F;
 
     // Start is called before the first frame update
     void Start()
@@ -21,19 +22,21 @@ private float speed = 20000;
     // Update is called once per frame
     void Update()
     {
-
-
-    }
-
-    private void Movement()
-    {
+        //KEYBOARD MOVEMENT
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
+
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.up * Time.deltaTime * speed * horizontalInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+
+        //CAMERA
+        float h = mouseHorizontalSpeed * Input.GetAxis("Mouse X");
+        transform.Rotate(0, h, 0);
 
     }
-}
 
 
+
+} //fucking vores alles projekt
