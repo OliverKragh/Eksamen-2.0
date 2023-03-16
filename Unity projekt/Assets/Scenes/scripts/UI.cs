be95f08396274c71810993673c4f2e9546cf57ae
JeppeMW <jeppewortmann@outlook.dk>
2023-03-16 08:33:40 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/OliverKragh/Eksamen-2.0""
    This reverts commit 4ca35fa801d3187222681ce18731b818c69576ea.

---
+++ Unity projekt/Assets/Scenes/scripts/UI.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Button startKnap;
+    public Button indstillingerKnap;
+    
+    public GameObject indstillingerMenu;
+    public Button indstillingerMenuBack;
+    
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public void StartButtonClicked()
+    {
+        Debug.Log("Spil startet");
+        //Load Spil Scene
+       SceneManager.LoadScene("Spil Scenen", LoadSceneMode.Single);
+       //Unload UI Scene
+        SceneManager.UnloadSceneAsync("UI Scene");
+    }
+   
+   public void IndstillingerButtonClicked()
+    {
+        Debug.Log("Indstillinger din nød");
+        startKnap.gameObject.SetActive(false);
+        indstillingerKnap.gameObject.SetActive(false);
+        indstillingerMenu.gameObject.SetActive(true);
+        indstillingerMenuBack.gameObject.SetActive(true);
+
+    }
+
+    public void indstillingerMenuBackClicked()
+    {
+        startKnap.gameObject.SetActive(true);
+        indstillingerKnap.gameObject.SetActive(true);
+        indstillingerMenu.gameObject.SetActive(false);
+    }
+}
