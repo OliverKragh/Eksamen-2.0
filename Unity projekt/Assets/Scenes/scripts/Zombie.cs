4ca35fa801d3187222681ce18731b818c69576ea
JeppeMW <jeppewortmann@outlook.dk>
2023-03-16 08:30:11 +0100

    Revert "Merge branch 'main' of https://github.com/OliverKragh/Eksamen-2.0"
    This reverts commit a76d7354bbcae345f4f31bb30912cb69efe30e39, reversing
    changes made to 88552f3f18653a4a923011f6681a3d893d8c4b11.

--- Unity projekt/Assets/Scenes/scripts/Zombie.cs
+++ Unity projekt/Assets/Scenes/scripts/Zombie.cs
@@ -4,64 +4,15 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
-
-    public int startingHealth = 100;
-    private int currentHealth;
-    private Rigidbody zombieRB;
-     private Transform playerRB;
-    private GameObject player;
-    private float zombieSpeed = 1;
-
-    public int chance;
-    private int number;
-
     // Start is called before the first frame update
     void Start()
     {
-        zombieRB = GetComponent<Rigidbody>();
-        player = GameObject.Find("MaleFree1");
-        currentHealth = startingHealth;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Zombier følger spiller
-        Vector3 targetDirection = (player.transform.position - transform.position).normalized;
-       
-        transform.LookAt(player.transform);
-        transform.Translate(Vector3.forward * Time.deltaTime * zombieSpeed );
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        currentHealth -= 20;
-            Debug.Log("RAMT");
-
-            if (currentHealth <= 0)
-            {
-                Die();
-            }
-        Destroy(other);
-
         
-        if (other.CompareTag("Bullet"))
-        {
-            Debug.Log("SKU DA RAMT AF EN DIMS");
-        }
-    }
-
-    void Die()
-    {
-        Destroy(gameObject);
-
-        number = Random.Range(1, chance);
-        if (number == 1)
-        {
-            Debug.Log("POWERUP");
-        }
-
     }
 }
-
-
